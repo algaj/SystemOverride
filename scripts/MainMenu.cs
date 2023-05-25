eaf9c924e28f06bdb6e7e5dc971e16d7f9198229
algaj <ales.gajdacz@gmail.com>
2023-05-25 11:00:52 +0200

    Fix main menu script

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -8,9 +8,6 @@ public partial class MainMenu : Control
 	Button newGameButton;
 
 	[Export]
-	Button settingsButton;
-
-	[Export]
 	Button creditsButton;
 
 	[Export]
@@ -22,21 +19,16 @@ public partial class MainMenu : Control
     [Export]
     PackedScene creditsScene;
 
-    [Export]
-    PackedScene settingsScene;
 
 
 	public override void _Ready()
 	{
 		Debug.Assert(newGameButton != null, "newGameButton != null");
-		Debug.Assert(settingsButton != null, "settingsButton != null");
 		Debug.Assert(creditsButton != null, "creditsButton != null");
 		Debug.Assert(exitButton != null, "exitButton != null");
         Debug.Assert(creditsScene != null, "creditsScene != null");
-        Debug.Assert(settingsScene != null, "settingsScene != null");
 
         newGameButton.ButtonDown += NewGameButton_ButtonDown;
-        settingsButton.ButtonDown += SettingsButton_ButtonDown;
         creditsButton.ButtonDown += CreditsButton_ButtonDown;
         exitButton.ButtonDown += ExitButton_ButtonDown;
 	}
@@ -49,11 +41,6 @@ public partial class MainMenu : Control
     private void CreditsButton_ButtonDown()
     {
         GetTree().ChangeSceneToPacked(creditsScene);
-    }
-
-    private void SettingsButton_ButtonDown()
-    {
-        GetTree().ChangeSceneToPacked(settingsScene);
     }
 
     private void NewGameButton_ButtonDown()
